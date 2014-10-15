3cb2198ef245b864d62900b67cac4391325f1000
Bastian E. <baschtel101@gmail.com>
2014-10-15 19:17:51 +0200

    umstellung auf Localhost

---
+++ config.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("VERSION","0.0.1");
+define("VERSION_TEXT","Rockscheune");
+define("HOST", "localhost");
+define("USER", "root");
+define("DATABASE", "scheune");
+define("PASSWORD", "");
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+//Tabellen
+define("T_TERMINE","termine");
+
+//Ordner
+define("DIR_ROOT", "/");
+
+//Bilder
+define("IMG_LOGO", "img/logo.png");
+?>
