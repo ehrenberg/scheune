e48aacfb1e0440cc5c407b5794929437ae6e88aa
Bastian E. <baschtel101@gmail.com>
2014-10-15 21:07:51 +0200

    Termine auslesen
    erste Versuche, die Termine der aktuellen Woche auszulesen..

--- termine.php
+++ termine.php
@@ -1,96 +1,86 @@
 <?php
-	require_once('db_connect.php');
-	require_once('Functions.inc.php');
-	
-	//Ermitteln des Wochenanfangs und Ende
-	$jahr				= Date("Y");
-	$kalenderwoche		= strftime("%V");
-	$timestamp_montag	= strtotime("{$jahr}-W{$kalenderwoche}");
-	$timestamp_sonntag	= strtotime("{$jahr}-W{$kalenderwoche}-7");
-	
-	$tabelle_termine = null;
+require_once('db_connect.php');
+require_once('class/Template.class.php');
+include_once('Functions.inc.php');
 
+//Ermitteln des Wochenanfangs und Ende
+$inhalt				= null;
+$jahr				= Date("Y");
+$kalenderwoche		= strftime("%V");
+$tabelle_termine	= null;
+
+
+$monday		= new DateTime('last monday');
+$sunday		= new DateTime('next sunday');
+
+$cols	= array("ID", "Text", "Von", "Bis");
+$db->where("Von", $monday->format('d.m.Y H:i:s'), ">");
+$db->where("Bis", $sunday->format('d.m.Y H:i:s'), "<");
+$termine	= $db->get(T_TERMINE, null, $cols);
+
+foreach ($termine as $termin) { 
+	print_r ($user);
+	echo '123';
+	/*
+	$inhalt .= getWochenTag(strtotime($Von));
+	$inhalt .= getWochenTag(strtotime($Bis));
+	$i++;
 	
-	$sql = "SELECT ID, Text, Von, Bis
-			FROM ".T_TERMINE." T
-			WHERE Von > '".date('Y-m-d H:i:s', $timestamp_montag)."'
-			AND Bis < '".date('Y-m-d H:i:s', $timestamp_sonntag)."'
-			ORDER BY T.Von ASC";
-	if (!$stmt = $mysqli->prepare($sql)) {
-		echo $mysqli->error;		
-	}
+	if($i == 1)$wochentag = 'Montag';
+	else if($i == 2)$wochentag = 'Dienstag';
+	else if($i == 3)$wochentag = 'Mittwoch';
+	else if($i == 4)$wochentag = 'Donnerstag';
+	else if($i == 5)$wochentag = 'Freitag';
+	else if($i == 6)$wochentag = 'Samstag';
+	else if($i == 7)$wochentag = 'Sonntag';
 	
-	if (!$stmt->execute()) {
-		echo $stmt->error;
-	}
+	$tabelle_termine .= '<tr>
+							<td>'.date("d.m.Y",strtotime($Von)).'</td>
+							<td>'.$wochentag.'</td><td>'.$Text.'</td>
+							<td>'.date("H:i",strtotime($Von)).'</td>
+							<td>'.date("H:i",strtotime($Bis)).'</td>
+						</tr>';
+						*/
+}
+
+$tabelle_termine .= '<table>';
+
+
+$i = 0;
+/*
+while ($stmt->fetch()) {
+	$inhalt .= getWochenTag(strtotime($Von));
+	$inhalt .= getWochenTag(strtotime($Bis));
+	$i++;
 	
-	$stmt->bind_result($ID,$Text,$Von,$Bis);
+	if($i == 1)$wochentag = 'Montag';
+	else if($i == 2)$wochentag = 'Dienstag';
+	else if($i == 3)$wochentag = 'Mittwoch';
+	else if($i == 4)$wochentag = 'Donnerstag';
+	else if($i == 5)$wochentag = 'Freitag';
+	else if($i == 6)$wochentag = 'Samstag';
+	else if($i == 7)$wochentag = 'Sonntag';
 	
-	echo getWochenTag(strtotime($Von));
-	
-	$i = 0;
-	while ($stmt->fetch())
-	{
-		$i++;
-		
-		if($i == 1)$wochentag = 'Montag';
-		else if($i == 2)$wochentag = 'Dienstag';
-		else if($i == 3)$wochentag = 'Mittwoch';
-		else if($i == 4)$wochentag = 'Donnerstag';
-		else if($i == 5)$wochentag = 'Freitag';
-		else if($i == 6)$wochentag = 'Samstag';
-		else if($i == 7)$wochentag = 'Sonntag';
-		
-		$tabelle_termine .= '<tr><td>'.date("d.m.Y",strtotime($Von)).'</td><td>'.$wochentag.'</td><td>'.$Text.'</td><td>'.date("H:i",strtotime($Von)).'</td><td>'.date("H:i",strtotime($Bis)).'</tr>';
-	}
+	$tabelle_termine .= '<tr>
+							<td>'.date("d.m.Y",strtotime($Von)).'</td>
+							<td>'.$wochentag.'</td><td>'.$Text.'</td>
+							<td>'.date("H:i",strtotime($Von)).'</td>
+							<td>'.date("H:i",strtotime($Bis)).'</td>
+						</tr>';
+}*/
+$tabelle_termine .= '</table>';
+
+$inhalt .= $tabelle_termine;
+
+/*
+ *	HAUPTGERÜST
+ */
+$Template	= new tpl("main.tpl");
+$sere = array (
+		"title"				=> "Der Schuppen - Termine",
+		"inhalt"			=> $inhalt
+		//"navigation"		=> create_Navigation($mysqli)
+);
+
+echo $Template->fill_tpl("start", $sere);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
-<head>
-	<title>Radio Rockscheune - Wenn`s nicht rockt ist es für`n Arsch</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<div id="container">
-	<div id="header">
-		<div id="status">
-			<iframe scrolling="no" class="playerstatus" src="http://status.streamplus.de/active2.php?serverid=24478&onlinecolor=black"></iframe>
-		</div>
-		<div class="clear"></div>
-		<div id="player">
-			<a href="http://streamplus18.leonex.de:23106/listen.pls"><img src="img/vlc-icon.png" width="40px" height="40px"><br/>Click`n Rock</a>
-		</div>
-		<div class="clear"></div>
-	</div>
-	<div id="navigation">
-		<ul>
-			<a href="/"><li>Startseite</li></a>
-			<a href="kontakt.php"><li>Kontakt</li></a>
-			<a href="termine.php"><li>Termine</li></a>
-			<a href="info.php"><li>Info</li></a>
-		</ul>
-		<div class="clear"></div>
-	</div>
-	<div id="content">
-		<p>Radio RockScheue 24 Stunden non stop werbefreie Edelware</p>
-		<br />
-		<table class="table_termine">
-			<?php
-				echo $tabelle_termine;
-			?>
-		</table>
-		<img src="img/no_limit.png">
-	</div>
-	
-	<div id="footer">
-		<ul>
-			<a href="impressum.php"><li>Impressum</li></a>
-		</ul>
-		<div class="clear"></div>
-	</div>
-</div>
-</body>
-
-</html>
