35bec51f2a54b4092e4d5f49c0d52699b71c8101
Bastian E. <baschtel101@gmail.com>
2014-10-15 21:07:28 +0200

    Umstellung auf Templates
    Seite laden nur noch Templates

--- index.php
+++ index.php
@@ -1,102 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
-<head>
-	<title>Radio Rockscheune - Wenn`s nicht rockt ist es für`n Arsch</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<div id="container">
-	<div id="header">
-		<div id="status">
-			<iframe scrolling="no" class="playerstatus" src="http://status.streamplus.de/active2.php?serverid=24478&onlinecolor=black"></iframe>
-		</div>
-		<div class="clear"></div>
-		<div id="player">
+<?php
+require_once('db_connect.php');
+require_once('class/Template.class.php');
+include_once('Functions.inc.php');
 
-<p>
-<br/>
-		Auf Sendung :   <br/>  Rock NonStop<br/>
-		<br/>
+$sere = array();
 
-<a href="http://login.streamplus.de/app.php/shoutcast/public/playlist/download/26174.m3u"><img src="img/radio.png"><br/>Zum Radio</a>
+/*
+ *	STARTSEITE
+ */
+$Template	= new tpl("start.tpl");
+$inhalt		= $Template->fill_tpl("start", $sere);
 
+/*
+ *	HAUPTGERÜST
+ */
+$Template	= new tpl("main.tpl");
+$sere = array (
+		"title"				=> "Der Schuppen - Termine",
+		"inhalt"			=> $inhalt
+		//"navigation"		=> create_Navigation($mysqli)
+);
 
-
-</p>
-		</div>
-		<div class="clear"></div>
-	</div>
-	<div id="navigation">
-		<ul>
-			<a href="/"><li>Startseite</li></a>
-			<a href="kontakt.php"><li>Kontakt</li></a>
-                        <a href="termine.php"><li>Termine</li></a>
-                        <a href="info.php"><li>Info</li></a>
-
-                         
-		</ul>
-		<div class="clear"></div>
-	</div>
-	
-	<div id="content">
-		Hallo,<br/>
-		<br/>
-		willkommen bei Radio RockScheune!<br/>
-		<br/>
-		Dem geilsten RockRadio in Niedersachsen ^^
-<br/>
-		24 Stunden non stop werbefreie Mucke !
-<br/>
-               100 % Musik und Fun auf die Lauschlappen.
-<br/>
-               ( Programm-/Konzerte-Infos auch immer aktuell bei spin.de,  Skype und Facebook. Radio RockScheune )
-<br/>
-<br/>
-
-
-
-
-			</tr>
-			<tr>
-			<td colspan="2"><br/>Edelware aus Classic -, Blues -, Country -, Mittelalter -  und HardRock !! </td><br/>
-<br/>
-
-
-<br/>
-Wir verzichten hier absichtlich auf Glitzerfummel und Werbebanner-Geblinke !!!  Here is Music,  simple... <br/>  The Home of Rock !! <br/>
-</tr>
-<br/>
-6 Millionen Menschen auf der Welt h&oumlren Hip Hop - schreib dich nicht ab - lern reden und gehen!<br/>
-
-*******************************************************************************
- <br/>
-</tr>
-
-
-<br/>
-<br/>
-<td><span <td><span class="highlight">Gr&uuml&szlige und W&uumlnsche gern auch &uumlber  WhatsApp  an:<br/><br/>
-<td><span <td><span class="highlight">0176 447 937 09 <br/><br/>
-		</table>
-		<br/><br/>
-<br/>
-
-		<table class="table_termine">
-		<thead>
-<br/>
-
-
-		Have Fun. Rock on!
-		<img src="img/hinweis-04.jpg">
-	<div id="footer">
-		<ul>
-			<a href="impressum.php"><li>Impressum</li></a>
-		</ul>
-		<div class="clear"></div>
-	</div>
-</div>
-</body>
-
-</html>
+echo $Template->fill_tpl("start", $sere);
+?>
--- kontakt.php
+++ kontakt.php
@@ -1,32 +1,21 @@
 <?php
+require_once('db_connect.php');
+require_once('class/Template.class.php');
+include_once('Functions.inc.php');
+
 if(isset($_GET['p'])) {
 	$p = $_GET['p'];
-	if($p == 'danke')$inhalt .= '<h1>Danke für deine Unterstützung</h1>';
-	else $inhalt = '';
+	if($p == 'danke') {
+		$inhalt .= '<h1>Danke für deine Unterstützung</h1>';
+	}
 }
-// An welche Adresse sollen die Mails gesendet werden?
-$zieladresse = 'mail@radio-rockscheune.de';
 
-// Welche Adresse soll als Absender angegeben werden?
-// (Manche Hoster lassen diese Angabe vor dem Versenden der Mail ueberschreiben)
-$absenderadresse = 'mail@radio-rockscheune.de';
-
-// Welcher Absendername soll verwendet werden?
-$absendername = 'Radio - Rockscheune';
-
-// Welchen Betreff sollen die Mails erhalten?
-$betreff = 'Feedback';
-
-// Zu welcher Seite soll als "Danke-Seite" weitergeleitet werden?
-// Wichtig: Sie muessen hier eine gueltige HTTP-Adresse angeben!
-$urlDankeSeite = 'kontakt.php?p=danke';
-
-// Welche(s) Zeichen soll(en) zwischen dem Feldnamen und dem angegebenen Wert stehen?
-$trenner = ":\t"; // Doppelpunkt + Tabulator
-
-/**
- * Ende Konfiguration
- */
+$zieladresse		= 'mail@radio-rockscheune.de';
+$absenderadresse	= 'mail@radio-rockscheune.de';
+$absendername		= 'Radio - Rockscheune';
+$betreff			= 'Feedback';
+$urlDankeSeite		= 'kontakt.php?p=danke';
+$trenner			= ":\t";
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
@@ -58,64 +47,25 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     exit;
 }
 
-header("Content-type: text/html; charset=utf-8");
 
+
+$sere = array();
+
+/*
+ *	STARTSEITE
+ */
+$Template	= new tpl("kontakt.tpl");
+$inhalt		= $Template->fill_tpl("start", $sere);
+
+/*
+ *	HAUPTGERÜST
+ */
+$Template	= new tpl("main.tpl");
+$sere = array (
+		"title"				=> "Der Schuppen - Kontakt",
+		"inhalt"			=> $inhalt
+		//"navigation"		=> create_Navigation($mysqli)
+);
+
+echo $Template->fill_tpl("start", $sere);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
-<head>
-	<title>Radio Rockscheune - Wenn`s nicht rockt ist es für`n Arsch</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<div id="container">
-	<div id="header">
-		<div id="status">
-			<iframe scrolling="no" class="playerstatus" src="http://status.streamplus.de/active2.php?serverid=24478&onlinecolor=black"></iframe>
-		</div>
-		
-		<div class="clear"></div>
-		<div id="player">
-			<a href="http://streamplus18.leonex.de:23106/listen.pls"><img src="img/vlc-icon.png"><br/>Click`n Rock</a>
-		</div>
-		<div class="clear"></div>
-	</div>
-	<div id="navigation">
-		<ul>
-			<a href="/"><li>Startseite</li></a>
-			<a href="kontakt.php"><li>Kontakt</li></a>
-			<a href="info.php"><li>Info</li></a>
-
-                        
-		</ul>
-		<div class="clear"></div>
-	</div>
-	
-	<div id="content">
-		<? echo $inhalt; ?>
-        <div style="float:right;top:0px;position:relative;">
-			<img src="img/made_in_de.png">
-		</div>
-		<form action="kontakt.php" method="post" class="contactform">
-            <dl>
-				<dt>Name:</dt>
-				<dd><input type="text" name="Versender" /></dd>
-                <dt></dt>
-                <dd><input type="radio" name="Geschlecht" value="Mann" />Mann <input type="radio" name="Geschlecht" value="Frau" />Frau</dd>
-                <dt>Grüße, Wünsche & Kritik: (oder bei Skype: Radio RockScheune)</dt>
-                <dd><textarea maxlength="500" name="Text" rows="8" cols="50"></textarea></dd>
-            </dl>
-            <p><input type="submit" value="Senden" /></p>
-        </form>
-		
-		<div class="clear"></div>
-		<div id="footer">
-			<ul>
-				<a href="impressum.php"><li>Impressum</li></a>
-			</ul>
-			<div class="clear"></div>
-		</div>
-    </body>
-</html>
